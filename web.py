3a769f4ee51260415861b60fedb802f4515b0c24
Bo Zhu <github@bozhu.me>
2013-10-04 23:32:52 -0400

    push to heroku

--- web.py
+++ web.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
 from flask import Flask, request, Response
 from twilio import twiml
 from twilio.rest import TwilioRestClient
+from raven.contrib.flask import Sentry
+from simsimi import SimSimi, SimSimiException
 
 
 app = Flask(__name__)
-sentry_client = None
-try:
-    from raven.contrib.flask import Sentry
-    sentry_client = Sentry(app, dsn=os.environ.get('SENTRY_DSN'))
-except:
-    pass
 
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+assert ACCESS_TOKEN
+
 TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER')
 twilio_client = TwilioRestClient(
     os.environ.get('TWILIO_SID'),
     os.environ.get('TWILIO_TOKEN')
 )
+sentry_client = Sentry(app, dsn=os.environ.get('SENTRY_DSN'))
+simsimi_client = SimSimi(os.environ.get('SIMSIMI_KEY'))
 
 
 @app.route('/' + ACCESS_TOKEN, methods=['GET'])
@@ -27,8 +28,13 @@ def handle_text(access_token):
     text_content = request.form['Body']
 
     resp = twiml.Response()
-    resp.message(text_content)
-    return Response(str(resp), mimetype='text/xml')
+    try:
+        simsimi_resp = simsimi_client.get_response(text_content)
+        resp.message(simsimi_resp)
+    except SimSimiException:
+        sentry_client.captureException()
+        resp.message(u'哦')
+    return Response(resp.toxml(), mimetype='text/xml')
 
 
 if __name__ == '__main__':
